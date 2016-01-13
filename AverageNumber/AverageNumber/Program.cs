39c5d42a8f6505efd893e7cddd0e85decd6b9e89
Joni Korkiakoski <k1755@student.jamk.fi>
2016-01-13 14:36:12 +0200

    AverageNumbers

---
+++ AverageNumber/AverageNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give second number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give third number");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((num1 + num2 + num3) / 3);
+            Console.ReadLine();
+
+        }
+    }
+}
