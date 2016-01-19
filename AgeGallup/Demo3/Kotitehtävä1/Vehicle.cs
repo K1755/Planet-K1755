e41a300d9db3e537b20b483d80fc21d006d7d111
Joni Korkiakoski <k1755@student.jamk.fi>
2016-01-20 01:33:49 +0200

    Update

---
+++ AgeGallup/Demo3/Kotitehtävä1/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtävä1
+{
+    class Vehicle
+    {
+        //Properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        //Method prints Vehicle properties
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle: ");
+            Console.WriteLine("Name: {0} | Speed: {1}km/h | Tyres: {2}", Name, Speed, Tyres);
+        }
+
+            //Method returns Vehicle properties
+            public override string ToString()
+        {
+            return "Name: " + Name + " | Speed: " + Speed + "km/h | Tyres: " + Tyres;
+        }
+    }
+
+}
