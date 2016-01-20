6232fe5b120434e87076f4a3ed6c568f111cb530
Joni Korkiakoski <k1755@student.jamk.fi>
2016-01-20 13:07:00 +0200

    lerppu

---
+++ AgeGallup/Demo4/CarApplication1/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication1
+{
+    class Car
+    {
+        //Max speed
+        private readonly int maxSpeed = 200;
+        //Member Field
+        private int speed;
+        //Property
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                if (value <= maxSpeed) speed = value;
+                else Console.WriteLine("Too much speed - set to max!");
+                speed = maxSpeed;
+            }
+        }
+    }
+}
