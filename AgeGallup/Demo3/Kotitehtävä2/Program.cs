1847b8b88f400ad18f4a9d3871f2a3ed53e368a4
Joni Korkiakoski <k1755@student.jamk.fi>
2016-01-20 06:49:30 +0200

    Update

---
+++ AgeGallup/Demo3/Kotitehtävä2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtävä2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut = new int[5];
+
+            Student[] students = Student[5];
+        }
+    }
+}
---
+++ AgeGallup/Demo3/Kotitehtävä2/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtävä2
+{
+    class Student
+    {
+
+        public struct Students
+        {
+            public string Names, Gender;
+            public int Age;
+        }
+        
+    }
+}
